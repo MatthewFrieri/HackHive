62e571745edb94ff6ace8a2d7284e60df2c46d8b
Jason Wang <jason.wang1709@gmail.com>
2026-01-24 19:49:44 -0500

    implement speech to text with llm filter and deterministic filter

---
+++ speech/rstt.py
@@ -0,0 +1,62 @@
+import os
+import threading
+import time
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+from RealtimeSTT import AudioToTextRecorder
+
+VOCAB = {
+    "raise": ['raise', 'grays', 'graze', 'gaze', 'greys'],
+    "fold": ["fold", "forward", "hold"],
+    "check": ['check'],
+    "call": ['call']
+}
+
+ACTION_MAP = {
+    "raise": 'R',
+    "fold": "F",
+    "check": "X",
+    "call": "C"
+}
+
+class STT:
+    def __init__(self):
+        self.recorder = AudioToTextRecorder()
+        self.current_action = None
+        self.current_action_time = time.time()
+        self.lock = threading.lock()
+
+    def get_state(self):
+        return (self.current_action_time, self.current_action)
+
+    def parse(self, text):
+        print(f"CURRENT ACTION: {self.current_action}")
+        print(f"RAW: {text}")
+        text = text.lower().replace(".", "").replace("'", "").replace("!", "")
+        print(f"PROCESSED: {text}")
+        action_emb = None
+        for action, near in VOCAB.items():
+            for tok in text.split():
+                if tok in near:
+                    action_emb = ACTION_MAP[action]
+                    if action == 'raise':
+                        tokens = text.split()
+                        numbers = [int(t) for t in tokens if t.isdigit()]
+                        amount = numbers[0] if numbers else None 
+                        action_emb += str(amount)
+        print(f"ACTION_EMB: {action_emb}")
+        with self.lock:
+            self.current_action_time = time.time()
+            self.current_action = action_emb
+    
+    def run(self):
+        try: 
+            while True:
+                self.recorder.text(self.parse)
+        except KeyboardInterrupt:
+            print("\nStopped")
+
+
+if __name__ == "__main__":
+    S = STT()
+    S.run()
